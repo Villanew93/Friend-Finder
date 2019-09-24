e444e59f18ce2162b88d5286222cfaf2beca75d2
isai villanueva~git configssh-keygen -t rsa -b 4096 -C isai.villanu.7@gmail.comgit config --global user.name isai <isai.villanu.7@gmail.com>
2019-09-24 13:01:51 -0700

    updtaing js

---
+++ app/data/friends.js
@@ -0,0 +1,169 @@
+var friends = [
+
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  
+  {
+    name: "Kakashi Hatake",
+    photo: "https://live.staticflickr.com/8470/8354141568_8af0399da7_b.jpg",
+    scores: [
+        '2',
+        '4',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '2',
+        '1',
+        '5'
+    ]
+},
+{
+    name: "Alec Baldwin",
+    photo: "https://cdn1.thr.com/sites/default/files/imagecache/NFE_portrait/2013/05/FN0A7516_seduced_and_abandoned.jpg",
+    scores: [
+        '4',
+        '3',
+        '5',
+        '1',
+        '1',
+        '5',
+        '5',
+        '1',
+        '1',
+        '1'
+    ]
+},
+{
+    name: "Daft Punk",
+    photo: "https://s3.amazonaws.com/factmag-images/wp-content/uploads/2017/01/Daft-Punk-shine-616x440.jpg",
+    scores: [
+        '5',
+        '1',
+        '2',
+        '1',
+        '3',
+        '4',
+        '1',
+        '3',
+        '3',
+        '4'
+    ]
+},
+{
+    name: "Ryan Gosling",
+    photo: "https://cdn.24.co.za/files/Cms/General/d/4901/c760f2c3630044ebabd6dea9fb111f7f.jpg",
+    scores: [
+        '1',
+        '1',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '1'
+    ]
+},
+{
+    name: "Jesus Christ",
+    photo: "https://assets.ldscdn.org/ef/32/ef32470c7c4bddbca31d997b8ec60e49266252df/jesus_sermon_mount.jpeg",
+    scores: [
+        '1',
+        '1',
+        '5',
+        '1',
+        '3',
+        '2',
+        '5',
+        '1',
+        '1',
+        '5'
+    ]
+},
+{
+    name: "Rihanna",
+    photo: "https://peopledotcom.files.wordpress.com/2019/09/rihanna-diamond-ball03.jpg",
+    scores: [
+        '5',
+        '4',
+        '4',
+        '2',
+        '4',
+        '4',
+        '3',
+        '2',
+        '3',
+        '3'
+    ]
+},
+{
+    name: "Poste Malone",
+    photo: "https://media.gq.com/photos/5a5f8929d97538770250dbe6/16:9/w_2560,c_limit/Post_Malone_Shot_02.jpg",
+    scores: [
+        '5',
+        '2',
+        '3',
+        '1',
+        '5',
+        '1',
+        '3',
+        '1',
+        '1',
+        '5'
+    ]
+},
+{
+    name: "Susan B. Anthony",
+    photo: "https://cdn.britannica.com/28/13428-050-37CEEE27/Susan-B-Anthony.jpg",
+    scores: [
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5'
+    ]
+},
+{
+    name: 'Elmo',
+    photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+    scores: [
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1'
+    ]
+}
+];
+
+module.exports = friends;
+  
+  
+  
